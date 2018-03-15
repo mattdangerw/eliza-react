2f1b28ee0c005ef287d5e7c8a87c7825b2f2d868
Matt Watson <mattdangerw@gmail.com>
2018-03-15 15:19:33 -0700

    Import assets through webpack

--- src/ChatMessage.js
+++ src/ChatMessage.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { Comment } from 'semantic-ui-react';
+import robotAvatar from './robot.svg';
+import humanAvatar from './human.svg';
 
 class ChatMessage extends Component {
   render() {
     const message = this.props.message;
-    let avatar = '/assets/images/robot.svg';
+    let avatar = robotAvatar;
     let author = 'ELIZA';
     let className = 'eliza';
     if (message.user) {
-      avatar = '/assets/images/human.svg';
+      avatar = humanAvatar;
       author = 'Me';
       className = 'user';
     }
